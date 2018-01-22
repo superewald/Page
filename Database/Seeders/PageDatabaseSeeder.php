33f8460e46da1ab456da7bac8288e174ddeafd7f
Mario Ewald <m.ewald@alerno.de>
2018-01-22 14:08:51 +0100

    Zwischencommit

--- Database/Seeders/PageDatabaseSeeder.php
+++ Database/Seeders/PageDatabaseSeeder.php
@@ -6,43 +6,44 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 use Modules\Page\Repositories\PageBlockRepository;
 use Modules\Page\Repositories\PageRepository;
+use Faker\Factory;
 
 class PageDatabaseSeeder extends Seeder
 {
-    /**
-     * @var PageRepository
-     */
-    private $page;
+	/**
+	 * @var PageRepository
+	 */
+	private $page;
 
-    private $block;
+	private $block;
 
-    private $faker;
+	private $faker;
 
-    public function __construct(PageRepository $page, PageBlockRepository $block)
-    {
-        $this->page = $page;
-        $this->block = $block;
-        $this->faker = Faker\Factory::create();
-    }
+	public function __construct(PageRepository $page, PageBlockRepository $block)
+	{
+		$this->page = $page;
+		$this->block = $block;
+		$this->faker = Factory::create();
+	}
 
-    public function run()
-    {
-        Model::unguard();
+	public function run()
+	{
+		Model::unguard();
 
-        $data = [
-            'template' => 'home',
-            'is_home' => 1,
-            'en' => [
-                'title' => 'Home page',
-                'slug' => 'home',
-                'meta_title' => 'Home page',
-            ],
-        ];
+		$data = [
+			'template' => 'home',
+			'is_home' => 1,
+			'en' => [
+				'title' => 'Home page',
+				'slug' => 'home',
+				'meta_title' => 'Home page',
+			],
+		];
 
-        $page = $this->page->create($data);
+		$page = $this->page->create($data);
 
-	    for($x = 0; $x < 5; $x++)
-	    {
+		for($x = 0; $x < 5; $x++)
+		{
 			$blockData = [
 				'en' => [
 					'title' => $this->faker->name,
@@ -50,8 +51,8 @@ class PageDatabaseSeeder extends Seeder
 				],
 			];
 
-			$block = $this->block->create($data);
-			$page->blocks()->attach([$block->id]);
-	    }
-    }
+			$block = $this->block->create($blockData);
+			$page->blocks()->attach($block->id);
+		}
+	}
 }
---
+++ Entities/PageBlockType.php
@@ -0,0 +1,12 @@
+<?php
+namespace Modules\Page\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Dimsav\Translatable\Translatable;
+
+class PageBlockType extends Model
+{
+	protected $table = "page_block_type";
+
+
+}
